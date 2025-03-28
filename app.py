6dff71bfec1b4d8e2f8c81e7d2dc6539d6a64a26
NoPlanToLand44 <emil.shupev@gmail.com>
2025-03-28 17:02:28 +0200

    got a working localhost server with FLASK that is displaying my form

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 import datetime
 import os
-from backtest_module import run_backtest # TODO implement this function in price_data.py
+# from backtest_module import run_backtest # TODO implement this function in price_data.py
 
 app = Flask(__name__)
-app.route('/', methods=['GET', "POST"])
+@app.route('/', methods=['GET', "POST"])
 def index():
     if request.method == 'POST':
         # Extract data from inputs from the HTML form 
@@ -13,11 +13,12 @@ def index():
         end_date = request.form['end_date']
         capital = float(request.form['capital'])
         strategy = request.form['strategy']
-        
+
         #run the strategy 
         # results = run_backtest(ticker, start_date, end_date, capital, strategy) // import this function from price_data.py
         return render_template('results.html', results=results, ticker = ticker, start_date = start_date, end_date = end_date, capital = capital)
-    return render_template('index.html')
-
-
-
+    if request.method == 'GET':
+        # Render the initial form
+        return render_template('index.html')
+if __name__ == "__main__":
+    app.run(debug=True)
